43d66d86ac3a9298b5cb5cf551b2d53c67bff5ea
Lars Berntzon <lars.berntzon@external.atlascopco.com>
2022-10-25 16:00:37 +0200

    Some minor spellings

--- eca/__main__.py
+++ eca/__main__.py
@@ -5,10 +5,23 @@ from eca.timestamp import TimestampDB
 import logging
 import sys
 
+def escape(s):
+    ret = ""
+    for c in s:
+        if c.isprintable():
+            ret += c
+        elif c <= '\xff':
+            ret += r'\x{0:02x}'.format(ord(c))
+        else:
+            ret += c.encode('unicode_escape').decode('ascii')
+    return ret
+
 def main():
     if len(sys.argv) != 2:
         raise RuntimeError("Usage: eca <yaml-config-file>")
     config = eca.config.Config(sys.argv[1])
+
+    logging.basicConfig(level=logging.WARNING)
 
     # Collect all incident timestamps.
     timestamps: TimestampDB = TimestampDB(range=config.range)
@@ -27,27 +40,30 @@ def main():
             if timestamps.is_applicable(ts):
                 if timestamps.in_range(ts):
                     texts_within[line] += 1
-                    logging.debug(f"inside:  {ts} - {line}")
+                    logging.debug(f"inside:  {ts} - {line[:80]}")
                 else:
                     texts_outside[line] += 1
-                    logging.debug(f"outside: {ts} - {line}")
+                    logging.debug(f"outside: {ts} - {line[:80]}")
             else:
                 texts_not_applicable[line] += 1
 
     # Find texts that only occur close to incident events but not otherwise and has at least
     # percentile percent number of hits from total incident events.
-    min_count = (len(timestamps) * config.percentile) / 100
-    print("Text only occuring during incident events:")
-    print("------------------------------------------")
+    min_count = int((len(timestamps) * config.percentile) / 100)
+    logging.info(f"min_count:{min_count}")
+    print("Normalized texts occurring during incident event zone that matches accuracy and percentile settings:")
+    print("---------------------------------------------------------------------------------------------------")
     for txt, count in sorted(texts_within.items(), key=lambda x: x[1], reverse=True):
-        total_occurences = texts_outside[txt] + count
+        printable = escape(txt)
+        total_occurrences = texts_outside[txt] + count
+        logging.info(f"{printable[20:60]} : total_occurrences:{total_occurrences} count:{count}")
 
-        if (count * 100) / total_occurences >= config.accuracy and count >= min_count:
-            print(f"count {count}: {txt}")
-    print("------------------------------------------")
+        if int((count * 100) / total_occurrences) >= config.accuracy and count >= min_count:
+            print(f"within zone:{count} occurrences, out of zone: {texts_outside[txt]}:\n{printable}\n")
+    print("---------------------------------------------------------------------------------------------------")
 
-    print(f"total {len(timestamps)} incidents, "
-          f"{len(texts_within)} unique texts found within event range, "
+    print(f"Total {len(timestamps)} incidents, "
+          f"{len(texts_within)} texts found within event range, "
           f"{len(texts_outside)} outside. "
           f"{len(texts_not_applicable)} not applicable.")
 
